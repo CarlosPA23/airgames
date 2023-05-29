08d933c45b3ceb28c3fd7894d282d6b644012ddc
CarlosPeña <karlos.alonsop@gmail.com>
2023-05-29 17:51:56 +0200

    Created the new and create routes, controllers and views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,27 @@
 class GamesController < ApplicationController
+  before_action :set_user, only: %i[new create]
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.user = @user
+    if @game.save
+      redirect_to user_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def game_params
+    params.require(:game).permit(:title, :description, :price, :category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'games/new', to: 'games#new', as: 'new_game'
+  post 'games', to: 'games#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
