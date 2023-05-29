b8041a0660970d4aa04f85d0d345f36093ff9bb6
marien irzykiewicz <marienirzyk@gmail.com>
2023-05-29 17:29:15 +0200

    created the edit and update on game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,12 @@
 class GamesController < ApplicationController
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    # No need for app/views/restaurants/update.html.erb
+    redirect_to game_path(@game)
+  end
 end
