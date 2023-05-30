5803443021c4ca4aef0274bc965ce57225f63aa0
Harris Grant <harris@shoptreen.com>
2023-05-30 15:54:11 +0200

    added reviews controller and pundit policy updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
   devise_for :users
   root to: "pages#home"
 
-  resources :games
+  resources :games do
+    resources :reviews, only: %i[new create]
+  end
 end
