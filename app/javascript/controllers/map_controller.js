f6a6e2db794276356e37155aedf35b4d4551872d
Harris Grant <harris@shoptreen.com>
2023-06-01 11:34:50 +0200

    added address autocomplete to game new and game edit pages

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,5 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="map"
 export default class extends Controller {
   connect() {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
   }
 }
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
+  validates :address, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
