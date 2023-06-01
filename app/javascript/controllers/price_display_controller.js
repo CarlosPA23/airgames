74b8851dbec3d028223e7a7f102a1b561c9179a4
marien irzykiewicz <marienirzyk@gmail.com>
2023-06-01 15:27:18 +0200

    calendar retro end date fixed, price per hour, minimum to to rent 1 hour, start date is proposed to user as today hoour now

--- app/javascript/controllers/price_display_controller.js
+++ app/javascript/controllers/price_display_controller.js
@@ -13,13 +13,23 @@ export default class extends Controller {
     console.log(this.priceValue);
   }
 
+  // calculate(event) {
+  //   console.log( typeof (Date.parse(this.endDateTarget.value) - Date.parse(this.startDateTarget.value)));
+  //   console.log( Math.floor((Date.parse(this.endDateTarget.value) - Date.parse(this.startDateTarget.value)) / 86400000));
+  //   const days = Math.floor((Date.parse(this.endDateTarget.value) - Date.parse(this.startDateTarget.value)) / 86400000)
+  //   const totalPrice = days * this.priceValue
+  //   this.priceTarget.innerText = totalPrice
+  // }
+
   calculate(event) {
-    console.log( typeof (Date.parse(this.endDateTarget.value) - Date.parse(this.startDateTarget.value)));
-    console.log( Math.floor((Date.parse(this.endDateTarget.value) - Date.parse(this.startDateTarget.value)) / 86400000));
-    const days = Math.floor((Date.parse(this.endDateTarget.value) - Date.parse(this.startDateTarget.value)) / 86400000)
-    const totalPrice = days * this.priceValue
-    this.priceTarget.innerText = totalPrice
+    const startDate = new Date(this.startDateTarget.value);
+    const endDate = new Date(this.endDateTarget.value);
+    const differenceInMilliseconds = endDate - startDate;
+    const differenceInHours = differenceInMilliseconds / 1000 / 60 / 60; // convert milliseconds to hours
+    const totalPrice = differenceInHours * this.priceValue;
+    this.priceTarget.innerText = totalPrice;
   }
+
 
   // initFlatpicker() {
   //   flatpickr(this.startDateTarget, {
@@ -33,16 +43,41 @@ export default class extends Controller {
   //   });
   // }
 
+  // initFlatpicker() {
+  //   let endDatePicker;
+
+  //   flatpickr(this.startDateTarget, {
+  //     minDate: "today",
+  //     dateFormat: "Y-m-d",
+  //     onChange: (selectedDates, dateStr, instance) => {
+  //       if (selectedDates.length > 0) {
+  //         let date = new Date(selectedDates[0]);
+  //         date.setDate(date.getDate() + 1);
+  //         if (endDatePicker) {
+  //           endDatePicker.set('minDate', date);
+  //         }
+  //       }
+  //     },
+  //   });
+
+  //   endDatePicker = flatpickr(this.endDateTarget, {
+  //     minDate: new Date().fp_incr(1), // initially set minDate to tomorrow
+  //     dateFormat: "Y-m-d",
+  //   });
+  // }
+
   initFlatpicker() {
     let endDatePicker;
 
     flatpickr(this.startDateTarget, {
-      minDate: "today",
-      dateFormat: "Y-m-d",
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+      minuteIncrement: 30,
+      minDate: new Date(), // sets minimum date and time to current time
       onChange: (selectedDates, dateStr, instance) => {
         if (selectedDates.length > 0) {
           let date = new Date(selectedDates[0]);
-          date.setDate(date.getDate() + 1);
+          date.setHours(date.getHours() + 1);
           if (endDatePicker) {
             endDatePicker.set('minDate', date);
           }
@@ -51,11 +86,10 @@ export default class extends Controller {
     });
 
     endDatePicker = flatpickr(this.endDateTarget, {
-      minDate: new Date().fp_incr(1), // initially set minDate to tomorrow
-      dateFormat: "Y-m-d",
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+      minuteIncrement: 30,
     });
   }
-
-
 
 }
