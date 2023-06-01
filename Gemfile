3125eb0a411adbf6da72139e56acab691fd85a0a
marien irzykiewicz <marienirzyk@gmail.com>
2023-06-01 11:47:01 +0200

    search in game

--- Gemfile
+++ Gemfile
@@ -53,6 +53,10 @@ gem "bootsnap", require: false
 # Use Sass to process CSS
 gem "sassc-rails"
 
+# Use to have the search feature
+gem "pg_search"
+
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,7 @@
+# require 'pg_search'
+
 class Game < ApplicationRecord
+
   belongs_to :user
 
   has_many :bookings, dependent: :destroy
@@ -7,4 +10,11 @@ class Game < ApplicationRecord
   has_one_attached :photo
   has_many :reviews, dependent: :destroy
 
+  # code to Searching One model using PgSearch GEM module
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_category
+    against: [ :title , :category ]
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
