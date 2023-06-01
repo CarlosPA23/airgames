04e5b20d638f24954f4007592a16b2ffed7e90b1
Harris Grant <harris@shoptreen.com>
2023-06-01 11:10:30 +0200

    added mapbox api key to .ENV file

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,9 @@
 class Game < ApplicationRecord
   belongs_to :user
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
 
